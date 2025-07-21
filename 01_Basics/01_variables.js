4f173a2f6126c4ed864625e69fb1ec85e4e32e8a
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2025-07-21 15:35:46 +0530

    studied variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "ishan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Raipur"
+let accountState
+
+accountEmail = "chandrakr@gmail.com"
+accountPassword = "67890"
+accountCity = "Durg"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// var is not scope based whereas let is
+// we can assign variable without const and let, but it should be avoided
+// undefined value is assigned to empty variables
