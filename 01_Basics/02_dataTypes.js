b4cc9df6e11821de0524ac77830ee73b3e4f83df
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2025-07-21 16:23:37 +0530

    studied datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+// number = 2 ^ 53
+// bigint = for large numbers
+// string = "assembly of characters"
+// boolean = true or false
+// null = standalone value/ empty value (type = object)
+// undefined = value is not defined yet (type = undefined)
+// symbol = uniquness
+
+// object
