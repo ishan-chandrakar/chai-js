fc58416ebfed64ea70b659a42cb1bd09eb57e46e
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2025-07-25 20:41:36 +0530

    date and time

---
+++ 01_Basics/08_date&time.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+
+// type of Date is Object
+
+let createDate = new Date(2025, 3, 4, 4, 0)
+// console.log(createDate.toString());
+//  month starts from 0
+
+let anotherMyDate = new Date("2000-08-22")
+// console.log(anotherMyDate.toLocaleString());
+
+let timeStamp = Date.now()
+// console.log(timeStamp);
+// console.log(myDate.getTime());
+
