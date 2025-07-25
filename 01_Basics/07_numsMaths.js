bfae82bd8e19c9bf0887e07f7300db3bb4f9955d
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2025-07-25 20:14:28 +0530

    Math and Numbers

---
+++ 01_Basics/07_numsMaths.js
@@ -0,0 +1,22 @@
+const balance = new Number(1234)
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+// console.log(balance.toFixed(2));
+
+// const anotherNumber = 23.3455
+// console.log(anotherNumber.toPrecision(1));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//                          Maths
+// console.log(Math);
+// console.log(Math.abs(-123.45));
+// console.log(Math.round(123.));
+// console.log(Math.min(3,4,6,8));
+// console.log(Math.max(3,4,6,8));
+
+const min= 10, max = 20
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
